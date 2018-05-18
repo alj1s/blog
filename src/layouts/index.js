ae5a25c111bfbd06f80ee2a0c8b8b4015e943834
Andrew Jones <ximenean@hotmail.com>
2018-05-18 21:14:31 +0100

    Remove bio

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -30,7 +30,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Andrew Jones
           </Link>
         </h1>
       )
@@ -51,7 +51,7 @@ class Template extends React.Component {
             }}
             to={'/'}
           >
-            Gatsby Starter Blog
+            Andrew Jones
           </Link>
         </h3>
       )
